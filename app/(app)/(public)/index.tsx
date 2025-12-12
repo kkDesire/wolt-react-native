c323e72838d53300ed1f7717f2f2cd601632ef56
Desire KOUASSI <kouamdesire90@gmail.com>
2025-12-12 12:15:49 +0000

    feat: implement Apple and Google authentication, restructure app routing, and add new fonts and UI dependencies.

---
+++ app/(app)/(public)/index.tsx
@@ -0,0 +1,115 @@
+import AppleAuthButton from "@/components/auth/AppleAuthButton";
+import GoogleAuthButton from "@/components/auth/GoogleAuthButton";
+import { Colors, Fonts } from "@/constants/theme";
+import { Image, Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
+
+export default function Index() {
+  const openWebBrowser = () => {
+    Linking.openURL('https://kdesire.vercel.app')
+  }
+  return (
+    <View
+      style={styles.container}
+    >
+      <View style={styles.infiniteScrollContainer}></View>
+      <View style={styles.contentContainer}>
+        <Image source={require('@/assets/images/wolt-logo.png')} style={styles.brandLogo} />
+        <Animated.Text
+          entering={FadeInDown}
+          style={styles.tagline}
+        >
+          Almost everything delivered
+        </Animated.Text>
+
+        {/* Login Buttons */}
+        <View style={styles.buttonContainer}>
+          <Animated.View entering={FadeInDown.delay(100)}>
+            <AppleAuthButton />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(200)}>
+            <GoogleAuthButton />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(300)}>
+            <TouchableOpacity style={styles.otherButton}>
+              <Text style={styles.otherButtonText}>Other options</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+        <Animated.View style={styles.privacyContainer} entering={FadeInDown.delay(400)}>
+          <Text style={styles.privacyText}>
+            Please visite {' '}
+            <Text style={styles.privacyLink} onPress={openWebBrowser}>Wolt Privacy Statement</Text>{' '}
+            To learn about personal data procssing at Wolt.
+          </Text>
+        </Animated.View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+  brandLogo: {
+    width: '100%',
+    height: 48,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  tagline: {
+    fontSize: 32,
+    fontFamily: Fonts.brandBlack,
+    textAlign: 'center',
+    marginBottom: 50,
+    lineHeight: 36,
+  },
+  buttonContainer: {
+    gap: 12,
+    width: '100%',
+  },
+  otherButton: {
+    backgroundColor: '#f0f0f0',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 17,
+    borderRadius: 12,
+    gap: 4,
+  },
+  otherButtonText: {
+    color: Colors.muted,
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  privacyContainer: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  privacyText: {
+    fontSize: 12,
+    color: '#999',
+    textAlign: 'center',
+    lineHeight: 18,
+  },
+  privacyLink: {
+    color: '#4285F4',
+    textDecorationLine: 'underline',
+  },
+  infiniteScrollContainer: {
+    flex: 0.8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 4,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+});
